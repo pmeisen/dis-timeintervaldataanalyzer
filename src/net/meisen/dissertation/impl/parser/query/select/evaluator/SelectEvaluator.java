c94a4adcb34b2283b9a3f8c652efb4066f43aee0
pmeisen <philipp@meisen.net>
2014-04-30 17:50:28 +0200

    - more JavaDoc

--- src/net/meisen/dissertation/impl/parser/query/select/evaluator/SelectEvaluator.java
+++ src/net/meisen/dissertation/impl/parser/query/select/evaluator/SelectEvaluator.java
@@ -4,13 +4,33 @@ import net.meisen.dissertation.impl.parser.query.select.SelectQuery;
 import net.meisen.dissertation.impl.time.series.TimeSeriesResult;
 import net.meisen.dissertation.model.data.TidaModel;
 
+/**
+ * An evaluator used to evaluate select queries.
+ * 
+ * @author pmeisen
+ * 
+ */
 public class SelectEvaluator {
 	private final TidaModel model;
 
+	/**
+	 * Standard constructor, initializes the evaluator to be used with the
+	 * specified {@code model}.
+	 * 
+	 * @param model
+	 *            the model to be used with the evaluator
+	 */
 	public SelectEvaluator(final TidaModel model) {
 		this.model = model;
 	}
 
+	/**
+	 * Evaluates the specified {@code SelectQuery}.
+	 * 
+	 * @param query
+	 *            the query to be evaluated
+	 * @return the result of the query
+	 */
 	public SelectResult evaluate(final SelectQuery query) {
 
 		// the result holder
--- src/net/meisen/dissertation/impl/parser/query/select/evaluator/SelectResult.java
+++ src/net/meisen/dissertation/impl/parser/query/select/evaluator/SelectResult.java
@@ -4,6 +4,14 @@ import net.meisen.dissertation.impl.parser.query.select.SelectQuery;
 import net.meisen.dissertation.impl.time.series.TimeSeriesResult;
 import net.meisen.dissertation.model.parser.query.IQueryResult;
 
+/**
+ * The result of the evaluation of a {@code SelectQuery}.
+ * 
+ * @see SelectQuery
+ * 
+ * @author pmeisen
+ * 
+ */
 public class SelectResult implements IQueryResult {
 	private final SelectQuery query;
 
@@ -11,35 +19,80 @@ public class SelectResult implements IQueryResult {
 	private GroupResult groupResult;
 	private TimeSeriesResult timeSeriesResult;
 
+	/**
+	 * Standard constructor which is used to specify the {@code query} this
+	 * result is valid for.
+	 * 
+	 * @param query
+	 *            the {@code SelectQuery} this result is valid for
+	 */
 	public SelectResult(final SelectQuery query) {
 		this.query = query;
 	}
 
-	public DescriptorLogicResult getFilterResult() {
-		return filterResult;
-	}
-
-	public void setFilterResult(final DescriptorLogicResult filterResult) {
-		this.filterResult = filterResult;
-	}
-
+	/**
+	 * Gets the query.
+	 * 
+	 * @return the query
+	 */
 	public SelectQuery getQuery() {
 		return query;
 	}
 
+	/**
+	 * Gets the result of the filter specified by the query.
+	 * 
+	 * @return the result of the filter specified by the query
+	 */
+	public DescriptorLogicResult getFilterResult() {
+		return filterResult;
+	}
+
+	/**
+	 * Sets the result of the filter specified by the query.
+	 * 
+	 * @param filterResult
+	 *            the result of the filtering
+	 */
+	public void setFilterResult(final DescriptorLogicResult filterResult) {
+		this.filterResult = filterResult;
+	}
+
+	/**
+	 * Sets the {@code TimeSeries} specified by the query.
+	 * 
+	 * @param timeSeriesResult
+	 *            the {@code TimeSeries} specified by the query
+	 */
 	public void setTimeSeriesResult(final TimeSeriesResult timeSeriesResult) {
 		this.timeSeriesResult = timeSeriesResult;
 	}
 
+	/**
+	 * Gets the determined {@code TimeSeries} specified by the query.
+	 * 
+	 * @return the determined {@code TimeSeries}
+	 */
 	public TimeSeriesResult getTimeSeriesResult() {
 		return timeSeriesResult;
 	}
 
+	/**
+	 * Gets the result of the group defined by the query.
+	 * 
+	 * @return the result of the group
+	 */
 	public GroupResult getGroupResult() {
 		return groupResult;
 	}
 
-	public void setGroupResult(GroupResult groupResult) {
+	/**
+	 * Sets the result of the group.
+	 * 
+	 * @param groupResult
+	 *            the grouping result
+	 */
+	public void setGroupResult(final GroupResult groupResult) {
 		this.groupResult = groupResult;
 	}
 }
