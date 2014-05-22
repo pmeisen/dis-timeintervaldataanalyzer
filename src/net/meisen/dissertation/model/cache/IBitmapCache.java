05032ebb6575619e07f21c5ddf6fddf0eb8b5f12
pmeisen <philipp@meisen.net>
2014-05-22 09:28:13 +0200

    - more JavaDoc

--- src/net/meisen/dissertation/model/cache/IBitmapCache.java
+++ src/net/meisen/dissertation/model/cache/IBitmapCache.java
@@ -1,20 +1,78 @@
 package net.meisen.dissertation.model.cache;
 
 import net.meisen.dissertation.model.data.TidaModel;
+import net.meisen.dissertation.model.indexes.BaseIndexFactory;
 import net.meisen.dissertation.model.indexes.datarecord.slices.Bitmap;
 import net.meisen.dissertation.model.indexes.datarecord.slices.BitmapId;
 
+/**
+ * A cache used to hold a specific part of bitmaps in memory and all bitmaps at
+ * a location specified by the concrete implementation (e.g. memory,
+ * file-system).
+ * 
+ * @author pmeisen
+ * 
+ */
 public interface IBitmapCache {
 
+	/**
+	 * Initializes the {@code BitmapCache} for the specified {@code model}.
+	 * 
+	 * @param model
+	 *            the {@code TidaModel} to initialize the cache for
+	 * 
+	 * @see TidaModel
+	 */
 	public void initialize(final TidaModel model);
 
+	/**
+	 * Configures the concrete {@code Cache} implementation.
+	 * 
+	 * @param configuration
+	 *            the configuration to be used for the concrete {@code Cache}
+	 *            implementation
+	 */
 	public void setConfig(final IBitmapCacheConfig configuration);
 
-	public void registerBitmapOwner(final IBitmapOwner user);
+	/**
+	 * Registers a {@code BitmapOwner} within the cache. The {@code BitmapOwner}
+	 * is the own getting informed if the {@code Bitmap} should be released from
+	 * memory (i.e. the garbage collection can take place).
+	 * 
+	 * @param owner
+	 *            the owner to be registered
+	 */
+	public void registerBitmapOwner(final IBitmapOwner owner);
 
+	/**
+	 * Caches the specified {@code bitmap} fpr the specified {@code bitmapId}.
+	 * 
+	 * @param bitmapId
+	 *            the {@code BitmapId} of the {@code bitmap} to be cached
+	 * @param bitmap
+	 *            the {@code Bitmap} to be cached
+	 * 
+	 * @see BitmapId
+	 * @see Bitmap
+	 */
 	public void cacheBitmap(final BitmapId<?> bitmapId, final Bitmap bitmap);
 
+	/**
+	 * Gets the {@code Bitmap} with the specified {@code bitmapId} from the
+	 * cache. This method never returns {@code null} instead a new
+	 * {@code Bitmap} should be created using
+	 * {@link BaseIndexFactory#createBitmap()} if no {@code Bitmap} is available
+	 * with the specified {@code BitmapId}.
+	 * 
+	 * @param bitmapId
+	 *            the {@code BitmapId} of the {@code Bitmap} to be retrieved
+	 * 
+	 * @return the {@code Bitmap} for the specified {@code BitmapId}
+	 */
 	public Bitmap getBitmap(final BitmapId<?> bitmapId);
 
+	/**
+	 * Releases all of the resources used by the cache.
+	 */
 	public void release();
 }
--- src/net/meisen/dissertation/model/cache/IBitmapOwner.java
+++ src/net/meisen/dissertation/model/cache/IBitmapOwner.java
@@ -2,9 +2,28 @@ package net.meisen.dissertation.model.cache;
 
 import net.meisen.dissertation.model.indexes.datarecord.slices.BitmapId;
 
+/**
+ * Marks an instance to be the owner of a {@code Bitmap}. There can only be one
+ * owner and the owner has the responsibility to release a {@code Bitmap} if
+ * asked for. Releasing means that the pointer to the bitmap is removed so that
+ * garbage collection can take place.
+ * 
+ * @author pmeisen
+ * 
+ */
 public interface IBitmapOwner {
 
+	/**
+	 * Gets the identifier of the {@code Bitmap} {@code this} owns.
+	 * 
+	 * @return the identifier of the {@code Bitmap} {@code this} owns
+	 */
 	public BitmapId<?> getBitmapId();
-	
+
+	/**
+	 * Informs the {@code BitmapOwner} to release the bitmap. Releasing means
+	 * that all pointers to the {@code Bitmap} are set to {@code null} so that
+	 * garbage collection can take place.
+	 */
 	public void releaseBitmap();
 }
