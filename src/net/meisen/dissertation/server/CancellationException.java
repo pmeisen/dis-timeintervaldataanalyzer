baae5f67ea051cdd1185fbc0014fdcc83192fa26
pmeisen <philipp@meisen.net>
2014-06-18 17:00:19 +0200

    - JavaDoc added

--- src/net/meisen/dissertation/server/CancellationException.java
+++ src/net/meisen/dissertation/server/CancellationException.java
@@ -1,5 +1,13 @@
 package net.meisen.dissertation.server;
 
+/**
+ * Exception thrown if a request is cancelled by the client.
+ * 
+ * @author pmeisen
+ * 
+ */
 public class CancellationException extends RuntimeException {
+	private static final long serialVersionUID = 2758451349612984298L;
 
+	// nothing
 }
