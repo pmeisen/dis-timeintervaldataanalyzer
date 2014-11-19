506c59502ee8c9ccaa471047a8dc5595317ed396
pmeisen <philipp@meisen.net>
2014-11-19 16:26:20 +0100

    - more cleaning

--- test/net/meisen/dissertation/impl/parser/query/TestDeleteQueries.java
+++ test/net/meisen/dissertation/impl/parser/query/TestDeleteQueries.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.text.ParseException;
 import java.util.Arrays;
 
 import net.meisen.dissertation.config.xslt.DefaultValues;
