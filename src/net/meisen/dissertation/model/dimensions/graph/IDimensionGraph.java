9b5e2a6dfe035e1ddb121fcc60cbc743079e2530
pmeisen <philipp@meisen.net>
2014-11-19 15:01:45 +0100

    - added JavaDoc

--- src/net/meisen/dissertation/model/dimensions/graph/IDimensionGraph.java
+++ src/net/meisen/dissertation/model/dimensions/graph/IDimensionGraph.java
@@ -7,6 +7,14 @@ import net.meisen.dissertation.model.dimensions.IDimension;
 import net.meisen.dissertation.model.indexes.datarecord.slices.Bitmap;
 import net.meisen.general.genmisc.exceptions.ForwardedRuntimeException;
 
+/**
+ * An internal graph representation of a dimension.
+ * 
+ * @author pmeisen
+ * 
+ * @see IDimension
+ * 
+ */
 public interface IDimensionGraph {
 
 	/**
@@ -21,11 +29,37 @@ public interface IDimensionGraph {
 	public void create(final IDimension dimension)
 			throws ForwardedRuntimeException;
 
+	/**
+	 * Gets the dimension the graph belongs to.
+	 * 
+	 * @return the dimension the graph belongs to
+	 */
 	public IDimension getDimension();
 
+	/**
+	 * Resolved the {@code filterExpression} and creates the resulting bitmap.
+	 * 
+	 * @param filterExpression
+	 *            the expression
+	 * @param model
+	 *            the {@code TidaModel} used
+	 * 
+	 * @return the bitmap
+	 */
 	public Bitmap createFilter(final String filterExpression,
 			final TidaModel model);
 
+	/**
+	 * Resolved the {@code groupExpression} and creates the resulting bitmaps
+	 * for each group.
+	 * 
+	 * @param groupExpression
+	 *            the expression
+	 * @param model
+	 *            the {@code TidaModel} used
+	 * 
+	 * @return the bitmap
+	 */
 	public Map<String, Bitmap> createGroup(final String groupExpression,
 			final TidaModel model);
 
