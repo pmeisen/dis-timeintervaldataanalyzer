46884e081cd79d78f92c5013fb67992c20d2aa3a
pmeisen <philipp@meisen.net>
2014-11-20 08:33:45 +0100

    - added JavaDoc

--- src/net/meisen/dissertation/model/dimensions/DescriptorHierarchyManager.java
+++ src/net/meisen/dissertation/model/dimensions/DescriptorHierarchyManager.java
@@ -11,14 +11,28 @@ import net.meisen.general.genmisc.exceptions.ForwardedRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Manager used to manage the different {@code DescriptorHierarchy} instances.
+ * The manager manages the hierarchies of one specific
+ * {@code DescriptorDimension}.
+ * 
+ * @author pmeisen
+ * 
+ */
 public class DescriptorHierarchyManager {
 	private final static Logger LOG = LoggerFactory
 			.getLogger(DescriptorHierarchyManager.class);
 
 	private final DescriptorDimension descriptorDimension;
-
 	private final Map<String, DescriptorHierarchy> hierarchies;
 
+	/**
+	 * Constructor specifying the {@code DescriptorDimension} the manager
+	 * manages the hierarchies for.
+	 * 
+	 * @param descriptorDimension
+	 *            the {@code DescriptorDimension} the manager belongs to
+	 */
 	public DescriptorHierarchyManager(
 			final DescriptorDimension descriptorDimension) {
 		this.descriptorDimension = descriptorDimension;
@@ -26,6 +40,20 @@ public class DescriptorHierarchyManager {
 		this.hierarchies = new HashMap<String, DescriptorHierarchy>();
 	}
 
+	/**
+	 * Adds a hierarchy to the manager. The manager creates the hierarchy if
+	 * there is no other hierarchy using the specified {@code id}.
+	 * 
+	 * @param id
+	 *            the identifier of the hierarchy to be added
+	 * @param name
+	 *            the name of the hierarchy to be added
+	 * 
+	 * @return the created and added hierarchy
+	 * 
+	 * @throws ForwardedRuntimeException
+	 *             if an hierarchy with the specified identifier already exists
+	 */
 	public DescriptorHierarchy addHierarchy(final String id, final String name)
 			throws ForwardedRuntimeException {
 
@@ -48,10 +76,24 @@ public class DescriptorHierarchyManager {
 		}
 	}
 
+	/**
+	 * Gets all the defined hierarchies.
+	 * 
+	 * @return all the defined hierarchies
+	 */
 	public Collection<DescriptorHierarchy> getHierarchies() {
 		return Collections.unmodifiableCollection(hierarchies.values());
 	}
 
+	/**
+	 * Gets the hierarchy with the specified {@code hierarchyId}.
+	 * 
+	 * @param hierarchyId
+	 *            the identifier of the hierarchy to retrieve
+	 * 
+	 * @return the {@code DescriptorHierarchy} associated, can be {@code null}
+	 *         if no such hierarchy exists
+	 */
 	public DescriptorHierarchy getHierarchy(final String hierarchyId) {
 		return hierarchies.get(hierarchyId);
 	}
